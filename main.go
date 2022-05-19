4989691122381044747a85d34e25e46d03a8a593
Parrthu <105834566+Parrthu@users.noreply.github.com>
2022-05-19 13:06:13 +0800

    Update main.go

--- main.go
+++ main.go
@@ -10,8 +10,6 @@ import (
 	"time"
 
 	"github.com/Parrthu/ZeroBot-Plugin-SIKADI-/kanban" 
-	
-	"github.com/Parrthu/ZeroBot-Plugin-SIKADI-/kanban"
 	// 在最前打印 banner
 
 	// ---------以下插件均可通过前面加 // 注释，注释后停用并不加载插件--------- //
